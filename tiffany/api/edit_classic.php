65fb474ca55d0e74e0b42ff5aad8ddaf0bbfe207
maggiechen <chenmaggiejob@gmail.com>
2024-08-07 16:01:18 +0800

    debug

--- tiffany/api/edit_classic.php
+++ tiffany/api/edit_classic.php
@@ -1,9 +1,9 @@
 <?php
 include_once "base.php";
 $Classic = new DB('tf_classic');
-if ($_POST['sh'] == 1) {
-$Classic->q("UPDATE tf_classic SET sh = '0' WHERE id != '{$_POST['id']}'");
-}
+// if ($_POST['sh'] == 1) {
+// $Classic->q("UPDATE tf_classic SET sh = '0' WHERE id != '{$_POST['id']}'");
+// }
 // dd($_POST);
 // dd($_FILES);
 if (!empty($_FILES['img']['tmp_name'])) {
